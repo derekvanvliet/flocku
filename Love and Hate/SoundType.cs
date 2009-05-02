5eda57a04275a755a8cf87d394feff62acfd0526
r9segal <r9segal@9a69198c-3730-11de-8e0c-53a27eea117e>
2009-05-02 20:08:57 +0000

    Sound files

---
+++ Love and Hate/SoundType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace flocku
+{
+    public class SoundType
+    {
+        public WaveBank  wb;
+        public SoundBank sb;
+        public Song      sg;
+
+        public SoundType(WaveBank wb)
+        {
+            this.wb = wb;
+        }
+
+        public SoundType(SoundBank sb)
+        {
+            this.sb = sb;
+        }
+
+        public SoundType(Song sg)
+        {
+            this.sg = sg;
+        }
+    }
+}
