98226c6a2b41c343398b30b8d3a74edea82717f6
r9segal <r9segal@9a69198c-3730-11de-8e0c-53a27eea117e>
2009-05-02 20:08:57 +0000

    Sound files

---
+++ trunk/Love and Hate/SoundEx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+    public class SoundEx
+    {
+        public enum eSoundType
+        {
+            LOOP,
+            KILL
+        }
+
+        public eSoundType mSoundType = eSoundType.KILL;
+
+        public eSoundType SoundType
+        {
+            get { return mSoundType; }
+            set { mSoundType = value; }
+        }
+
+        public SoundEx(eSoundType eType)
+        {
+
+        }
+    }
+}
---
+++ trunk/Love and Hate/SoundManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using flocku;
+using System.Diagnostics;
+
+namespace WindowsGame1
+{
+    class SoundManager
+    {
+        static SoundManager me = new SoundManager();
+        static Dictionary<String, SoundType> mSounds = new Dictionary<string, SoundType>();
+
+        private AudioEngine mAudioEngine;
+
+        public AudioEngine Engine
+        {
+            get { return mAudioEngine; }
+        }
+
+        public static SoundManager Instance
+        {
+            get { return me; }
+        }
+
+        public void Initialize(ContentManager contentMgr, String sAssetName)
+        {
+            mAudioEngine = new AudioEngine(contentMgr.RootDirectory + sAssetName);
+        }
+
+        // Load content in the game project!
+        public bool Play(String sAsset)
+        {
+            try
+            {
+                if (!mSounds.ContainsKey(sAsset))
+                {
+                    throw new Exception("Asset " + sAsset + "does not exist!");
+                }
+
+                if (mSounds[sAsset].sg != null)
+                    MediaPlayer.Play(mSounds[sAsset].sg);
+
+                else if (mSounds[sAsset].sb != null)
+                    mSounds[sAsset].sb.PlayCue(sAsset);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+
+            return true;
+        }
+
+        public void Update()
+        {
+            mAudioEngine.Update();
+        }
+
+        public void Register(String sAssetName, Song aSound)
+        {
+            mSounds[sAssetName] = new SoundType(aSound);
+        }
+
+        public void Register(String sAsset, WaveBank aBank)
+        {
+            mSounds[sAsset] = new SoundType(aBank);
+        }
+
+        public void Register(String sAsset, SoundBank aBank)
+        {
+            mSounds[sAsset] = new SoundType(aBank);
+        }            
+    }
+}
